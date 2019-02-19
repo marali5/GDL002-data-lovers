1e1c21385b62e45fd2f695a13b2c7ee9f3ecb77c
Lucero Dn <lucero_dn@hotmail.es>
2019-02-19 08:05:43 -0600

    Se agrego el titulo, subtitulo y botones de tipo de pokemon con imagen en html y css

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const button1 = (data, water) => {
+    POKEMON.pokemon.lenght;
+    pokemon[i]= water;}
+
+    arrWater.push=pokemon[i].none;
+    arrWater.push=pokemon[i].image;
+ }
+
+ for (let i = 0; i < pokemon.length; i++) {
+    
